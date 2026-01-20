de463083c9fcf099baf362629f447be35b97ebe3
MatyiFKBT <6183867+MatyiFKBT@users.noreply.github.com>
2026-01-20 11:59:21 +0100

    ci: release

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,17 @@
 import { defineConfig } from 'vite';
 import monkey from 'vite-plugin-monkey';
-
-// https://vitejs.dev/config/
+import { author, name } from './package.json';
 export default defineConfig({
   plugins: [
     monkey({
       entry: 'src/main.ts',
       userscript: {
-        icon: 'https://vitejs.dev/logo.svg',
-        namespace: 'npm/vite-plugin-monkey',
+        icon: 'https://www.freecodecamp.org/favicon-32x32.png',
+        namespace: 'matyifkbt/fcc-helper',
         match: ['https://www.freecodecamp.org/*'],
+        version: `v${new Date().getTime()}`,
+        downloadURL: `https://github.com/${author.name}/${name}/releases/download/latest/index.user.js`,
+        updateURL: `https://github.com/${author.name}/${name}/releases/download/latest/index.meta.js`,
       },
     }),
   ],
